73a8407c5c908065f2572814fbb6cffa2229fa9d
MxPaPa <153317637+MxPaPa@users.noreply.github.com>
2023-12-14 00:18:06 +0600

    ha

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
 
 Route::controller(HomeController::class)->group(function () {
     Route::get('/', 'index');
